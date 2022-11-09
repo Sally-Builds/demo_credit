4f5a4f0267053957bf1f34b6d61067aba7b02ead
Sally-Builds <uzoagulujoshua@yahoo.com>
2022-11-09 14:49:36 +0100

    fix: use env port

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,24 @@
-import 'dotenv/config';
-import 'module-alias/register';
-import validateEnv from '@/utils/validateEnv';
-import App from './app';
+import 'dotenv/config'
+import 'module-alias/register'
+import validateEnv from '@/utils/validateEnv'
+import App from './app'
 
 // controller imports below
-import UserController from './resources/users/userController';
-import CreditController from './resources/transactions/credit/creditController';
-import WithdrawController from './resources/transactions/withdraw/withdrawController';
-import TransferController from './resources/transactions/transfer/transferController';
+import UserController from './resources/users/userController'
+import CreditController from './resources/transactions/credit/creditController'
+import WithdrawController from './resources/transactions/withdraw/withdrawController'
+import TransferController from './resources/transactions/transfer/transferController'
 
-validateEnv();
+validateEnv()
 
 const app = new App(
-    [
-        new UserController(),
-        new CreditController(),
-        new WithdrawController(),
-        new TransferController()
-    ],
-    Number(3000)
-);
+  [
+    new UserController(),
+    new CreditController(),
+    new WithdrawController(),
+    new TransferController()
+  ],
+  Number(process.env.PORT || 3000)
+)
 
-app.listen();
+app.listen()
