890fcc5b7935e3f46607f0c985e5db733a4554bb
Sally-Builds <uzoagulujoshua@yahoo.com>
2022-11-09 03:48:10 +0100

    fix[bug]: jwt malfunction refactor update

--- src/middleware/authenticate.middleware.ts
+++ src/middleware/authenticate.middleware.ts
@@ -1,25 +1,26 @@
 import  jwt from "jsonwebtoken";
 import { Request, Response, NextFunction } from "express";
-import {verifyToken} from '@/utils/token'
 import Token from '@/utils/interfaces/token.interface'
 import HttpException from '@/utils/exceptions/httpExceptions'
-import UserDB from '@/resources/users/userPersistence'
+import UserRepository from "@/resources/users/repository/userRepository";
+import {JwtAdapter} from "@/resources/users/factory/cryptography/jwtAdapter"
 
 async function authenticate (req:Request, res:Response, next:NextFunction): Promise<Response | void> {
     const bearer = req.headers.authorization
 
     if(!bearer || !bearer.startsWith('Bearer')){
-        return next(new HttpException('Unauthorized access', 401))
+     return next(new HttpException('Unauthorized access', 401))
     }
     const accessToken = bearer.split('Bearer ')[1].trim()
     try {
-        const payload: Token | jwt.JsonWebTokenError = await verifyToken(accessToken)
+        const payload: Token | jwt.JsonWebTokenError = await new JwtAdapter('secret', '30d').verify(accessToken)
+
 
         if(payload instanceof jwt.JsonWebTokenError) {
         return next(new HttpException('Unauthorized access', 401))
         }
 
-        const user = await new UserDB().getById(payload.id)
+        const user = await new UserRepository().getById(payload.id)
 
         if(!user) {
             return next(new HttpException('Unauthorized access', 401))
