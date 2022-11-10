3162b743853fd995a584239b08465704392b3eba
Sally-Builds <uzoagulujoshua@yahoo.com>
2022-11-10 11:05:55 +0100

    test: increase balance error check

--- tests/wallet/usecases/increaseBalance.spec.ts
+++ tests/wallet/usecases/increaseBalance.spec.ts
@@ -2,6 +2,7 @@ import { describe, expect, it, jest, afterEach } from '@jest/globals'
 import IncreaseBalanceUsecase from '../../../src/resources/wallet/usecase/increaseBalanceUsecase'
 import { UpdateBalanceRepoStub, GetBalanceRepoStub } from '../repositories'
 import walletMockDB from '../mockData/walletMockDB'
+import HttpException from '../../../src/utils/exceptions/httpExceptions'
 
 type SystemUnderTest = {
     sut: IncreaseBalanceUsecase,
@@ -37,5 +38,13 @@ describe('Should increase Wallet Balance', () => {
     const getBalanceRepoSpy = jest.spyOn(GetBalanceRepoStub, 'getBalance')
     await sut.execute({ credit_wallet: walletMockDB[0].account_no, amount: 10 })
     expect(await getBalanceRepoSpy.mock.results[0].value).toEqual(walletMockDB[0].balance - 10)
+  })
+})
+
+describe('Increase Balance should throw an Error', () => {
+  it('Get balance repository should throw an Error', async () => {
+    const { sut } = makeSut()
+    const val = async () => { await sut.execute({ credit_wallet: 'invalid_id', amount: 10 }) }
+    await expect(val()).rejects.toThrow(new HttpException('something went wrong', 500))
   })
 })
